b98726a89628e50bff741cf274150ad532076dad
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-12-07 11:40:25 -0300

    estudando vetor

---
+++ 001 - vetor/script.js
@@ -0,0 +1,19 @@
+//----------------------------------------------------
+let nome1 = ["Safira", "Rubi", "Esmeralda"]
+//traz todo elemento do array
+console.log(nome1)  
+//----------------------------------------------------
+let nome2 = ["Safira", "Rubi", "Esmeralda"]
+//traz nome na posição zero pode fazer com a posição [1] e [2]
+console.log(nome2[0])  
+//----------------------------------------------------
+let nome3 = ["Safira", "Rubi", "Esmeralda"]
+nome3.shift() //método shift remove o primeiro elemento do array
+console.log(nome3)
+//----------------------------------------------------
+let nome4 = ["Safira", "Rubi", "Esmeralda"]
+nome4.pop() //método pop remove o último elemento do array
+console.log(nome4)
+//----------------------------------------------------
+
+//OBSERVAÇÃO: Existem vários métodos que podem ser aplicados em array
