9a03ae63e269695f09bc29150f1d01f2d1e02770
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-12-08 19:19:26 -0300

    estudando matrizes

---
+++ 002 - matrizes/script.js
@@ -0,0 +1,42 @@
+// Vetores bidimencionais ou matrizes
+let tipoDeAnimais = [
+  ["Safira", "Parda", "Recem Nascida"],
+  ["Rubi", "Preto", "Não castrado"],
+  ["Esmeralda", "Branca", "Castrada"],
+];
+//propriedades length diz o tamanho da matriz
+console.log(tipoDeAnimais.length)
+
+//posição 0 na linha 0 = Safira
+console.log(tipoDeAnimais[0][0]);
+
+//posição 0 na linha 1 = Parda
+console.log(tipoDeAnimais[0][1]);
+
+//posição 0 na linha 2 = Recem Nascida
+console.log(tipoDeAnimais[0][2]);
+
+//posição 1 na linha 0 = Rubi
+console.log(tipoDeAnimais[1][0]);
+
+//posição 1 na linha 1 = Preto
+console.log(tipoDeAnimais[1][1]);
+
+//posição 1 na linha 2 = Não Castrada
+console.log(tipoDeAnimais[1][2]);
+
+//posição 2 na linha 0 = Esmeralda
+console.log(tipoDeAnimais[2][0]);
+
+//posição 2 na linha 1 = Branca
+console.log(tipoDeAnimais[2][1]);
+
+//posição 2 na linha 2 = Castrada
+console.log(tipoDeAnimais[2][2]);
+
+
+
+
+
+
+
