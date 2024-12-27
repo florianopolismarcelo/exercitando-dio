14da1b6cadc8193e096e18c8b8fd84666dc3eaa0
florianopolismarcelo <florianopolismarcelo@msn.com>
2024-12-27 13:34:47 -0300

    métodos e propriedades

--- 015 - classes-e-objetos/script.js
+++ 015 - classes-e-objetos/script.js
@@ -6,8 +6,23 @@ class formaDeBolo {
     this.saborDaMassa = saborDaMassa;
     this.saborRecheio = saborRecheio;
   }
+  //Abaixo função, método como está dentro de uma classe não precisa usar na frente a palavra function
+  escrever() {
+    console.log(
+      `Um delicioso bolo com ${this.saborDaMassa} e recheio de ${this.saborRecheio}`
+    );
+  }
+  assar() {
+    console.log("assando bolo de " + this.saborDaMassa);
+  }
 }
 //Abaixo Objeto
-let boloFesta = new formaDeBolo("massa de chocolate", "recheio de nutella");
+let boloFesta = new formaDeBolo("massa de chocolate", "nutella");
+let boloPremium = new formaDeBolo("baunilha", "coco");
+boloFesta.escrever();
+boloPremium.escrever();
+boloFesta.assar();
 
-console.log(boloFesta);
+//console.log(boloFesta);
+//console.log(boloFesta.saborRecheio);
+//console.log(boloFesta.saborDaMassa);
